8f182dfcce720e64b68de9d5d99e2f2b12b8e766
Chaitanya Senapati <csenapati12@gmail.com>
2020-06-20 22:24:39 +0530

    Create init-dev.sh

---
+++ init-dev.sh
@@ -0,0 +1,5 @@
+gcloud init
+wget https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_amd64.zip
+unzip terraform_0.12.24_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+rm terraform_0.12.24_linux_amd64.zip
